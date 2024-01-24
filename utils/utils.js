39651e12180a968c4c3c9efd2f11c239e2c67f9f
raazmi <mail.miraaz@gmail.com>
2024-01-24 13:58:08 +0600

    merge conflict update

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 const path = require("path");
 
 export const generateAltFromFilename = (filename) => {
-	return path.parse(filename).name.replaceAll("-", " ").replaceAll(/ +/g, " ")
+	return path.parse(filename).name.replaceAll("-", " ").replaceAll(/ +/g, " ");
 };
