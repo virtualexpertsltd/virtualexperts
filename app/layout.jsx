b5459a0b90ea7c5dd180322b794a4e57d2c36e96
Shah Bakhteyar Haider <haider.bakhteyar786@gmail.com>
2024-03-29 22:06:23 +0600

    add GTM

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,54 +1,56 @@
-import SiteFooter from "@/appComponents/layout/SiteFooter";
-import SiteHeader from "@/appComponents/layout/SiteHeader";
-import { Poppins, Roboto } from "next/font/google";
-import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+import SiteFooter from '@/appComponents/layout/SiteFooter';
+import SiteHeader from '@/appComponents/layout/SiteHeader';
+import { Poppins, Roboto } from 'next/font/google';
+import { GoogleTagManager } from '@next/third-parties/google';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 // ignore-organize-import
-import "@/styles/appGlobals.css";
+import '@/styles/appGlobals.css';
 
 export const metadata = {
-	title: {
-		template: "%s | VirtualExperts",
-		default: "VirtualExperts",
-	},
-	description:
-		"Stand Out from the Crowd and Maximize Your Profits with Virtual Experts Proven Method for Designing Amazon Listing Images that Turn Clicks into Sales.",
-	keyword:
-		"amazon fba consultant, amazon marketing service, amazon seo consultant, amazon seo agency, amazon listing optimisation, amazon listing images, amazon, ppc agency, amazon ppc services\n",
+  title: {
+    template: '%s | VirtualExperts',
+    default: 'VirtualExperts',
+  },
+  description:
+    'Stand Out from the Crowd and Maximize Your Profits with Virtual Experts Proven Method for Designing Amazon Listing Images that Turn Clicks into Sales.',
+  keyword:
+    'amazon fba consultant, amazon marketing service, amazon seo consultant, amazon seo agency, amazon listing optimisation, amazon listing images, amazon, ppc agency, amazon ppc services\n',
 };
 
 const roboto = Roboto({
-	weight: ["400", "500", "700"],
-	style: ["normal"],
-	subsets: ["latin"],
-	display: "swap",
-	fallback: ["system-ui", "arial"],
-	variable: "--font-roboto",
+  weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['system-ui', 'arial'],
+  variable: '--font-roboto',
 });
 
 const poppins = Poppins({
-	weight: ["400", "500", "600", "700"],
-	style: ["normal"],
-	subsets: ["latin"],
-	display: "swap",
-	fallback: ["system-ui", "arial"],
-	variable: "--font-poppins",
+  weight: ['400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['system-ui', 'arial'],
+  variable: '--font-poppins',
 });
 
 const RootLayout = ({ children }) => {
-	return (
-		<html lang="en" className={`${roboto.variable} ${poppins.variable} font-sans`}>
-			<body>
-				<div className="site-wrapper mt-[60px] md:mt-[80px]">
-					<SiteHeader />
-					{children}
-					<SiteFooter />
-				</div>
-				<div id="modal-root"></div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={`${roboto.variable} ${poppins.variable} font-sans`}>
+      <GoogleTagManager gtmId="GTM-MMLGVSGZ" />
+      <body>
+        <div className="site-wrapper mt-[60px] md:mt-[80px]">
+          <SiteHeader />
+          {children}
+          <SiteFooter />
+        </div>
+        <div id="modal-root"></div>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,63 +1,63 @@
-import jwt_decode from "jwt-decode";
-import Router from "next/router";
-import nProgress from "nprogress";
-import React, { createContext, useEffect, useState } from "react";
-import { ToastContainer } from "react-toastify";
-import Footer from "../Components/Shared/Footer/Footer";
-import Navbar from "../Components/Shared/Navbar/Navbar";
-import "../styles/globals.css";
+import jwt_decode from 'jwt-decode';
+import Router from 'next/router';
+import nProgress from 'nprogress';
+import React, { createContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { GoogleTagManager } from '@next/third-parties/google';
+import Footer from '../Components/Shared/Footer/Footer';
+import Navbar from '../Components/Shared/Navbar/Navbar';
+import '../styles/globals.css';
 
 nProgress.configure({ showSpinner: false });
 
-Router.events.on("routeChangeStart", () => nProgress.start());
-Router.events.on("routeChangeComplete", () => nProgress.done());
-Router.events.on("routeChangeError", () => nProgress.done());
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 export const UserContext = createContext();
 
 const CommonContent = ({ children }) => {
-	const [signedUser, setSignedUser] = useState({});
+  const [signedUser, setSignedUser] = useState({});
 
-	useEffect(() => {
-		const info = JSON.parse(window.localStorage.getItem("info"));
-		try {
-			const decoded = jwt_decode(info);
-			setSignedUser(decoded);
-		} catch (err) {}
-	}, []);
+  useEffect(() => {
+    const info = JSON.parse(window.localStorage.getItem('info'));
+    try {
+      const decoded = jwt_decode(info);
+      setSignedUser(decoded);
+    } catch (err) {}
+  }, []);
 
-	return (
-		<React.Fragment>
-			<UserContext.Provider value={[signedUser, setSignedUser]}>
-				{children}
-			</UserContext.Provider>
-			<ToastContainer
-				position="top-right"
-				autoClose={3000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-			/>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <UserContext.Provider value={[signedUser, setSignedUser]}>{children}</UserContext.Provider>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </React.Fragment>
+  );
 };
 
 export default function App({ Component, pageProps }) {
-	return (
-		<CommonContent>
-			{Component.getLayout ? (
-				Component.getLayout(<Component {...pageProps} />)
-			) : (
-				<React.Fragment>
-					<Navbar />
-					<Component {...pageProps} />
-					<Footer />
-				</React.Fragment>
-			)}
-		</CommonContent>
-	);
+  return (
+    <CommonContent>
+      {Component.getLayout ? (
+        Component.getLayout(<Component {...pageProps} />)
+      ) : (
+        <React.Fragment>
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </React.Fragment>
+      )}
+      <GoogleTagManager gtmId="GTM-MMLGVSGZ" />
+    </CommonContent>
+  );
 }
